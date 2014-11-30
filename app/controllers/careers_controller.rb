48773a80bd4aacada1bc3ed6bdd846c183483fe7
karynntran <karynn.tran@gmail.com>
2014-11-30 16:22:53 -0500

    Update links from homepage using jquery

---
+++ app/controllers/careers_controller.rb
@@ -0,0 +1,7 @@
+class CareersController < ApplicationController
+
+  def index
+    render :layout => 'application'
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'career/index'
+  resources :careers, only: [:index]
 
   root 'home#index'
 
