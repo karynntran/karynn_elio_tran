1395550c47d932f711834a8205517ec87d029a72
karynntran <karynn.tran@gmail.com>
2014-12-22 12:09:55 -0500

    Update routes and controller methods

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,22 @@
 class HomeController < ApplicationController
   layout 'application'
 
+  def career
+    render 'career/index'
+  end
+
+  def capture
+    render 'capture/index'
+  end
+
+  def create
+    render 'create/index'
+  end
+
+  def code
+    render 'code/index'
+  end
+
   def index
     render :layout => 'application'
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
+  get '/create' => 'home#create'
 
+  get '/capture' => 'home#capture'
+
+  get '/code' => 'home#code'
+
+  get '/career' => 'home#career'
 
   root 'home#index'
 
